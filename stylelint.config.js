0a7e499662056f15b604031474dd0f2b5b286395
Thibaud Colas <thibaudcolas@gmail.com>
2019-08-29 00:15:16 +0100

    chore(qa): configure stylelint to allow given font-size in code blocks

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   // https://github.com/thibaudcolas/stylelint-config-cookbook
   extends: "@thibaudcolas/stylelint-config-cookbook",
+  rules: {
+    "scale-unlimited/declaration-strict-value": [
+      ["/color/", "z-index", "font-size", "font-family"],
+      {
+        // We need to have a special keyword to ignore hard-coded font sizes in code samples.
+        ignoreKeywords: ["currentColor", "inherit", "transparent", "0.7625rem"],
+      },
+    ],
+  },
 }
